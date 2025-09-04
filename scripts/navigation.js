b9ea7f04b8c1905ea0c9b173e93e8453e4a7773a
luroblesf <rob23047@byui.edu>
2025-09-03 18:31:29 -0600

    w01

---
+++ scripts/navigation.js
@@ -0,0 +1,8 @@
+const navButton = document.querySelector('#nav-button');
+const navLinks = document.querySelector('#nav-bar');
+
+navButton.addEventListener('click', () => {
+  navButton.classList.toggle('show');
+  navLinks.classList.toggle('show');
+});
+
