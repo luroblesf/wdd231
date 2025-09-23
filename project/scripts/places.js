ef64513c01ce03c4e1f72563453078c2c5aab767
luroblesf <rob23047@byui.edu>
2025-09-22 18:54:53 -0600

    w06

---
+++ project/scripts/places.js
@@ -0,0 +1,37 @@
+const cards = document.querySelector('#cards');
+
+async function getPlacesData() {
+    const response = await fetch(url);
+    const data = await response.json();
+    displayProphets(data.places);
+}
+
+getPlacesData();
+
+const displayPlaces = (places) => {
+    places.forEach(place => {
+        let card = document.createElement('section');
+        let name = document.createElement('h2');
+        let portrait = document.createElement('img');
+        let schedule = document.createElement('p');
+        let phone = document.createElement('p');
+
+        name.textContent = `${places.name}`;
+        schedule.textContent = `Date of Birth: ${places.schedule}`;
+        phone.textContent = `Place of Birth: ${places.phone}`;
+
+        portrait.setAttribute('src', places.imageurl);
+        portrait.setAttribute('alt', `Portrait of ${places.name} ${places.schedule} - ${places.phone}`);
+        portrait.setAttribute('loading', 'lazy');
+        portrait.setAttribute('width', '340');
+        portrait.setAttribute('height', '440');
+
+        card.appendChild(name);
+        card.appendChild(schedule);
+        card.appendChild(phone);
+        card.appendChild(portrait);
+
+
+        cards.appendChild(card);
+    });
+}
